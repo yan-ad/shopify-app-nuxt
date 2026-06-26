149f9548187cff4d85f9754c45a81b4c72e05172
Yanuar <me@yan.ad>
2026-06-26 16:53:38 +0700

    feat: remove old author

--- docs/app/components/AppFooterLeft.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="text-sm text-muted">
-    © {{ new Date().getFullYear() }} KiriminAja. Polaris and Shopify are
-    trademarks of Shopify Inc.
-    <p class="text-xs">KiriminAja not affiliated with Shopify.</p>
-  </div>
-</template>
